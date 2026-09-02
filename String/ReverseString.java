99b4f1dd5daac9c001e48a2d3ac13d51ca5e26c6
Darsshini .R <darsshiniiragavan@gmail.com>
2026-09-02 07:47:38 +0530

    Strings updated

---
+++ String/ReverseString.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String result = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            result = result + str.charAt(i);
+        }
+
+        System.out.println(result);
+    }
+}
